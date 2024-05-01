a81d57341350aaa75516b4a5dfd7907b67b97327
OB1LAB <ob1chaam@gmail.com>
2024-05-01 12:59:47 +0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
+import { ToastContainer, toast } from "react-toastify";
+
+const urlSite = "Soon"
 
 function App() {
+  const copyUrlSte = () => {
+    navigator.clipboard.writeText(urlSite);
+    toast("Copied!", {type: "success"})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <ToastContainer
+        position="top-right"
+        autoClose={1500}
+        closeButton={true}
+        pauseOnHover={false}
+        theme="dark"
+      />
+      <div className="social">
+        <a href="/" target="_blank">
+          <img src="iconTg.webp" alt="telegram"/>
         </a>
-      </header>
+        <a href="/" target="_blank">
+          <img src="iconX.webp" alt="x"/>
+        </a>
+        <a href="/" target="_blank">
+          <img src="iconDex.webp" alt="dex"/>
+        </a>
+      </div>
+      <div className="pichi">
+        <h1>$pichi</h1>
+        <img src="panda.jpg" alt="panda"/>
+        <h2>I'm michi's friend at preschool, i'm shy 👉👈</h2>
+        <div className="link">
+          <div className="url">{urlSite}</div>
+          <button onClick={copyUrlSte}>copy</button>
+        </div>
+      </div>
+      <div className="memes">
+        <h1>pichi memes</h1>
+        <div>
+          <img src="JOKER.jpg" alt="JOKER"/>
+          <img src="LUFFY.png" alt="LUFFY"/>
+          <img src="NARUTO.png" alt="NARUTO"/>
+          <img src="CHIEF.png" alt="CHIEF"/>
+        </div>
+        <div>
+          <img src="GOKU.png" alt="GOKU"/>
+          <img src="MC.jpg" alt="MC"/>
+          <img src="PURPLE.jpg" alt="PURPLE"/>
+          <img src="CHINESE.png" alt="CHINESE"/>
+        </div>
+      </div>
     </div>
   );
 }
