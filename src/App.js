cdb03f77ff3c4948114f284a651c18ec40cdf6ca
OB1LAB <ob1chaam@gmail.com>
2024-05-01 17:30:03 +0300

    1.1 (Добавлен адаптив)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 import { ToastContainer, toast } from "react-toastify";
+import {useEffect, useLayoutEffect, useState} from "react";
 
-const urlSite = "Soon"
+const urlSite = "CA"
+
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
+
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false)
+  const [width] = useWindowSize();
   const copyUrlSte = () => {
     navigator.clipboard.writeText(urlSite);
     toast("Copied!", {type: "success"})
   }
+  useEffect(() => {
+    if (width < 850 && !isMobile) {
+      setIsMobile(true)
+    } else if(width >= 850 && isMobile) {
+      setIsMobile(false)
+    }
+  }, [width, isMobile]);
   return (
-    <div className="App">
+    <div className={"App"}>
       <ToastContainer
         position="top-right"
         autoClose={1500}
@@ -18,18 +43,18 @@ function App() {
         pauseOnHover={false}
         theme="dark"
       />
-      <div className="social">
-        <a href="/" target="_blank">
+      <div className={`${isMobile ? 'social mobile' : 'social'}`}>
+        <a href="https://t.me/pichisolanaportal" target="_blank" rel="noreferrer">
           <img src="iconTg.webp" alt="telegram"/>
         </a>
-        <a href="/" target="_blank">
+        <a href="https://twitter.com/pichionsolana" target="_blank" rel="noreferrer">
           <img src="iconX.webp" alt="x"/>
         </a>
-        <a href="/" target="_blank">
+        <a href="/" target="_blank" rel="noreferrer">
           <img src="iconDex.webp" alt="dex"/>
         </a>
       </div>
-      <div className="pichi">
+      <div className={`${isMobile ? 'pichi mobile' : 'pichi'}`}>
         <h1>$pichi</h1>
         <img src="panda.jpg" alt="panda"/>
         <h2>I'm michi's friend at preschool, i'm shy 👉👈</h2>
@@ -38,15 +63,15 @@ function App() {
           <button onClick={copyUrlSte}>copy</button>
         </div>
       </div>
-      <div className="memes">
+      <div className={`${isMobile ? 'memes mobile' : 'memes'}`}>
         <h1>pichi memes</h1>
-        <div>
+        <div className="memeBlock">
           <img src="JOKER.jpg" alt="JOKER"/>
           <img src="LUFFY.png" alt="LUFFY"/>
           <img src="NARUTO.png" alt="NARUTO"/>
           <img src="CHIEF.png" alt="CHIEF"/>
         </div>
-        <div>
+        <div className="memeBlock">
           <img src="GOKU.png" alt="GOKU"/>
           <img src="MC.jpg" alt="MC"/>
           <img src="PURPLE.jpg" alt="PURPLE"/>
